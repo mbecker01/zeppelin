084c2360e0d370c7975de45e0cf3bed60c56d106
mbecker01 <martin@becker-online.info>
2019-06-20 17:32:41 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 set -e
 
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://get.docker.com -o install_ecs.sh
-#   $ sh inctall_ecs.sh
-##
-
-# ssh -l linux 80.158.23.195 -i Downloads/KeyPair-Martin.pem
+#   $ curl -fsSL https://github.com/mbecker01/zeppelin/blob/master/install.sh -o install.sh | sh
+######
 
 yum -y update
 yum -y install yum-utils
@@ -20,6 +17,4 @@ systemctl start docker
 systemctl enable docker
 
 docker run --restart always -p 80:8080 skymindops/zeppelin-dl4j:latest 2>&1 >/dev/null
-
-echo "Docker Container started. Open http://IP in your browser."
 
